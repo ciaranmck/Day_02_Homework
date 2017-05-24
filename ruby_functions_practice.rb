197e4db5f55885fd7da7a2b09320c8b15ff34567
Ciaran McKenna <ciarantsc@gmail.com>
2017-05-24 10:04:16 +0100

    last tests passed using ifelse statements

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,15 +30,14 @@ def add_string_as_number(string_1, string_2)
   return string_1.to_i() + string_2.to_i()
 end
 
-def number_to_full_month_name(month)
- case month
-  when 1
-    return "January"
-  when 3 
-    return "March"
-  when 9
-    return "September"
-  end
+def number_to_full_month_name(number)
+ if number == 1 
+  return "January"
+elsif number == 3
+  return "March"
+elsif number == 9
+  return "September"
+end
 end
 
 def number_to_short_month_name(number)
